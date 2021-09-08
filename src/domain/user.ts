3f0a072b74eaf4625d73999f86f5a7abd3f89577
Roman Goldobin <rgoldobin@appliedtech.ru>
2021-09-08 16:22:33 +0500

    user description and common types

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,1 +1,12 @@
-export default {};
+export type UserName = string;
+export type UniqueId = string;
+export type Email = string;
+export type Ingredient = 'chocolate' | 'peanuts' | 'cocoa' | 'marshmallow' | 'cherry';
+
+export type User = {
+  id: UniqueId;
+  name: UserName;
+  email: Email;
+  preferences: Ingredient[];
+  allergies: Ingredient[];
+}
