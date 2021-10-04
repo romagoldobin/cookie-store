c6ecbbee2c24a538d202c65f11e05a0b53509aaf
romagoldobin <goldobinforwork@gmail.com>
2021-10-04 17:25:19 +0500

    services types(ports) definition

--- src/application/ports.ts
+++ src/application/ports.ts
@@ -1,1 +1,15 @@
-export default {};
+import { Order } from '../domain/order';
+import { PriceCents } from '../domain/product';
+
+export interface PaymentService {
+  tryPay(amout: PriceCents): Promise<boolean>;
+}
+
+export interface NotificationService {
+  notify(message: string): void;
+}
+
+export interface OrdersStorageService {
+  orders: Order[];
+  updateOrders(orders: Order[]): void;
+}
