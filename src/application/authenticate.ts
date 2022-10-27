fa02d976481c1f70104f3451b809dbc93e6f1b28
romagoldobin <goldobinforwork@gmail.com>
2022-10-27 23:36:34 +0500

    added useAuth hook

--- src/application/authenticate.ts
+++ src/application/authenticate.ts
@@ -1,1 +1,19 @@
-export default {};
+import { fakeAPI } from '../api';
+import { Email, UserName } from '../sharedTypes';
+import { AuthService } from './ports';
+
+
+
+export const useAuthenticate = (): AuthService => {
+  return {
+    auth(name: UserName, email: Email) {
+      return fakeAPI({
+        name,
+        email,
+        id: 'sample-user-id',
+        allergies: ['cocoa', 'cherry'],
+        preferences: ['marshmallow', 'peanuts'],
+      });
+    },
+  };
+};
